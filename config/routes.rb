45dfb54f6f83a78ad50786e8af9c35d556969825
Sergio Sosa Romero <smatias.sosa@gmail.com>
2023-06-10 05:34:04 +0200

    controller, routes and view created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  post '/calc', to: 'calculator#calculate'
+  get '/history', to: 'calculator#history'
+
   # Defines the root path route ("/")
+  root 'calculator#index'
   # root "articles#index"
 end
