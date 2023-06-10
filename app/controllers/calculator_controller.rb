0fa67309536da786a8b37872ccee69b7a41c41fc
Sergio Sosa Romero <smatias.sosa@gmail.com>
2023-06-10 05:50:53 +0200

    created model, add simple_form and actions in controller

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,2 +1,22 @@
 class CalculatorController < ApplicationController
+
+  class CalculatorController < ApplicationController
+    def index
+      # Render the form view
+    end
+
+    def calculate
+      arguments = params[:calc][:arg]
+      numeric_args = arguments.select { |arg| arg.to_i.to_s == arg || arg.to_f.to_s == arg }
+      result = numeric_args.sum(&:to_f)
+
+      # Save the query and result in the database
+      Calculation.create(query: arguments, result: result)
+
+      render plain: "Result: #{result}"
+    end
+  end
+
+
+
 end
