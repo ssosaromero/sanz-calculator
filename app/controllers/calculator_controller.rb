65a7581a07ab6199ed5717a7c62109d851ff496f
Sergio Sosa Romero <smatias.sosa@gmail.com>
2023-06-10 15:55:02 +0200

    clear values button added

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -5,17 +5,6 @@ class CalculatorController < ApplicationController
       # Render the form view
     end
 
-    # def calculate
-    #   arguments = params[:calc][:arg]
-    #   numeric_args = arguments.select { |arg| arg.to_i.to_s == arg || arg.to_f.to_s == arg }
-    #   result = numeric_args.sum(&:to_f)
-
-    #   # Save the query and result in the database
-    #   Calculation.create(arguments: arguments, result: result)
-
-    #   render plain: "Result: #{result}"
-    # end
-
     def calc
       arguments = params[:calc].values.map(&:to_f)
       numerical_arguments = arguments.select { |arg| arg.to_s =~ /^[0-9]+(\.[0-9]+)?$/ }
