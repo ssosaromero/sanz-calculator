c65f5d6ce141c0552185c8624c3b57b8f1d597ca
Sergio Sosa Romero <smatias.sosa@gmail.com>
2023-06-10 06:21:40 +0200

    Revert "actions added for history endpoint and includes operators on view and controller"
    This reverts commit 5e81de1fc07081d7416dd21cf1c1f3e9e3bd2389.

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,32 +1,22 @@
-# app/controllers/calculator_controller.rb
-
 class CalculatorController < ApplicationController
-  def calc
-    operator = params[:calculator][:operator]
-    arguments = params[:calculator][:arguments] || []
 
-    numerical_arguments = arguments.select { |arg| arg.is_a?(Numeric) }
+  class CalculatorController < ApplicationController
+    def index
+      # Render the form view
+    end
 
-    result = case operator
-             when '+'
-               numerical_arguments.sum
-             when '-'
-               numerical_arguments.reduce(:-)
-             when '*'
-               numerical_arguments.reduce(:*)
-             when '/'
-               numerical_arguments.reduce(:/)
-             else
-               nil
-             end
+    def calculate
+      arguments = params[:calc][:arg]
+      numeric_args = arguments.select { |arg| arg.to_i.to_s == arg || arg.to_f.to_s == arg }
+      result = numeric_args.sum(&:to_f)
 
-    Calculation.create(operator: operator, arguments: arguments, result: result)
+      # Save the query and result in the database
+      Calculation.create(query: arguments, result: result)
 
-    render json: { result: result }
+      render plain: "Result: #{result}"
+    end
   end
 
-  def history
-    calculations = Calculation.all
-    render json: { history: calculations }
-  end
+
+
 end
